3eda3d870ad4c854086d02a5d63e645c6cbaa39f
atla-naveen_bflghec <atla.naveen@bajajfinserv.in>
2025-09-28 13:06:03 +0530

    add consoles

--- Angular/portfolio-app/src/app/app.ts
+++ Angular/portfolio-app/src/app/app.ts
@@ -9,4 +9,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('portfolio-app');
+
+  constructor(){
+    console.log('added console');
+  }
 }
